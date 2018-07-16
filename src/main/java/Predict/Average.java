c7764ffdfb91c52ccd81a2481fa5665c7aab9434
rubychen0611 <rubychen0611@yeah.net>
2018-07-16 19:25:25 +0800

    Average finished

--- src/main/java/Predict/Average.java
+++ src/main/java/Predict/Average.java
@@ -1,19 +1,39 @@
 package Predict;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.net.URI;
 
 public class Average
 {
+    public static class LogTextOutputFormat extends TextOutputFormat<Text, Text>
+    {
+        @Override
+        public Path getDefaultWorkFile(TaskAttemptContext context, String extension) {
+            return new Path(getOutputName(context)+".txt");
+        }
+
+    }
     public static class AverageMapper extends Mapper<LongWritable, Text, Text, Text>
     {
         @Override
@@ -22,7 +42,7 @@ public class Average
             FileSplit fileSplit = (FileSplit)context.getInputSplit();
             String interfaceInfo = fileSplit.getPath().getName();    //得到文件名
             interfaceInfo = interfaceInfo.replace(".txt", "");  //去掉.txt.segmented
-            String[] line = value.toString().split(" ");
+            String[] line = value.toString().split("\t");
             if(line.length == 15)
             {
                 int sum = 0;
@@ -35,7 +55,7 @@ public class Average
             }
         }
     }
-    public static class LogAnalysisReducer extends Reducer<Text, Text, Text, Text>
+    public static class AverageReducer extends Reducer<Text, Text, Text, Text>
     {
         private MultipleOutputs<Text,Text> mos;
         private String outputPath;
@@ -50,12 +70,50 @@ public class Average
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
         {
             String keyInfo[] = key.toString().split("_");
-            mos.write("Average", new Text(keyInfo[1]),values.iterator() ,outputPath+"/"+keyInfo[0]);
+            mos.write("Average", new Text(keyInfo[1]),values.iterator().next(),outputPath+"/"+keyInfo[0]);
         }
         @Override
         public void cleanup(Context context) throws IOException, InterruptedException
         {
             mos.close();
+        }
+    }
+    public static void main(String[] args)
+    {
+        try{
+            // 若输出目录存在,则删除
+            Path path = new Path(args[1]);
+            FileSystem fileSystem = FileSystem.get(new URI(args[1]), new Configuration());
+            if (fileSystem.exists(path))
+                fileSystem.delete(path, true);
+
+            Configuration conf = new Configuration();
+            conf.set("outputPath", args[1]);
+            Job job = Job.getInstance(conf,"Average");
+
+
+            job.setJarByClass(Average.class);
+            job.setInputFormatClass(TextInputFormat.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(Text.class);
+
+            job.setMapperClass(Average.AverageMapper.class);
+            job.setReducerClass(Average.AverageReducer.class);
+
+            fileSystem = FileSystem.get(conf);
+            FileStatus[] fileStatusArray = fileSystem.globStatus(new Path(args[0]+"/*.txt"));
+            for(FileStatus fileStatus : fileStatusArray){
+                path = fileStatus.getPath();
+                FileInputFormat.addInputPath(job, path);
+            }
+            MultipleOutputs.addNamedOutput(job, "Average", LogTextOutputFormat.class, Text.class, Text.class);
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            LazyOutputFormat.setOutputFormatClass(job, LogTextOutputFormat.class);
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
